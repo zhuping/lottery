8c0d817c81107d1fed1fd374318ad4f2d9b6ffc4
无问 <zhuping@cai-inc.com>
2024-09-14 11:56:36 +0800

    fix: update

--- js/member.js
+++ js/member.js
@@ -1,254 +1,262 @@
 var member = [
   {
     "phone": "No.1",
-    "name": "幸运宝宝"
+    "name": "张宇豪"
   },
   {
     "phone": "No.2",
-    "name": "幸运宝宝"
+    "name": "穆柳竹"
   },
   {
     "phone": "No.3",
-    "name": "幸运宝宝"
+    "name": "蔡元凯"
   },
   {
     "phone": "No.4",
-    "name": "幸运宝宝"
+    "name": "赵子茜"
   },
   {
     "phone": "No.5",
-    "name": "幸运宝宝"
+    "name": "李万福"
   },
   {
     "phone": "No.6",
-    "name": "幸运宝宝"
+    "name": "宋新庆"
   },
   {
     "phone": "No.7",
-    "name": "幸运宝宝"
+    "name": "郭锦津"
   },
   {
     "phone": "No.8",
-    "name": "幸运宝宝"
+    "name": "李圳"
   },
   {
     "phone": "No.9",
-    "name": "幸运宝宝"
+    "name": "刘冠星"
   },
   {
     "phone": "No.10",
-    "name": "幸运宝宝"
+    "name": "楼雅玲"
   },
   {
     "phone": "No.11",
-    "name": "幸运宝宝"
+    "name": "胡奕"
   },
   {
     "phone": "No.12",
-    "name": "幸运宝宝"
+    "name": "王绵"
   },
   {
     "phone": "No.13",
-    "name": "幸运宝宝"
+    "name": "田建军"
   },
   {
     "phone": "No.14",
-    "name": "幸运宝宝"
+    "name": "卢皓玥"
   },
   {
     "phone": "No.15",
-    "name": "幸运宝宝"
+    "name": "陈琼妮"
   },
   {
     "phone": "No.16",
-    "name": "幸运宝宝"
+    "name": "李思昱"
   },
   {
     "phone": "No.17",
-    "name": "幸运宝宝"
+    "name": "舒苗苗"
   },
   {
     "phone": "No.18",
-    "name": "幸运宝宝"
+    "name": "王建"
   },
   {
     "phone": "No.19",
-    "name": "幸运宝宝"
+    "name": "胡嘉雯"
   },
   {
     "phone": "No.20",
-    "name": "幸运宝宝"
+    "name": "郭泰予"
   },
   {
     "phone": "No.21",
-    "name": "幸运宝宝"
+    "name": "汪玫"
   },
   {
     "phone": "No.22",
-    "name": "幸运宝宝"
+    "name": "黄舒颖"
   },
   {
     "phone": "No.23",
-    "name": "幸运宝宝"
+    "name": "张浩伦"
   },
   {
     "phone": "No.24",
-    "name": "幸运宝宝"
+    "name": "刘宇鹏"
   },
   {
     "phone": "No.25",
-    "name": "幸运宝宝"
+    "name": "胡鹏翼"
   },
   {
     "phone": "No.26",
-    "name": "幸运宝宝"
+    "name": "张坤"
   },
   {
     "phone": "No.27",
-    "name": "幸运宝宝"
+    "name": "盛梦洁"
   },
   {
     "phone": "No.28",
-    "name": "幸运宝宝"
+    "name": "姚艳苗"
   },
   {
     "phone": "No.29",
-    "name": "幸运宝宝"
+    "name": "陶朝兴"
   },
   {
     "phone": "No.30",
-    "name": "幸运宝宝"
+    "name": "朱姝娜"
   },
   {
     "phone": "No.31",
-    "name": "幸运宝宝"
+    "name": "陈彤彤"
   },
   {
     "phone": "No.32",
-    "name": "幸运宝宝"
+    "name": "杜泽轩"
   },
   {
     "phone": "No.33",
-    "name": "幸运宝宝"
+    "name": "王月"
   },
   {
     "phone": "No.34",
-    "name": "幸运宝宝"
+    "name": "孙钰英"
   },
   {
     "phone": "No.35",
-    "name": "幸运宝宝"
+    "name": "袁挺"
   },
   {
     "phone": "No.36",
-    "name": "幸运宝宝"
+    "name": "马春雪"
   },
   {
     "phone": "No.37",
-    "name": "幸运宝宝"
+    "name": "徐丽丽"
   },
   {
     "phone": "No.38",
-    "name": "幸运宝宝"
+    "name": "张丹"
   },
   {
     "phone": "No.39",
-    "name": "幸运宝宝"
+    "name": "吴士吉"
   },
   {
     "phone": "No.40",
-    "name": "幸运宝宝"
+    "name": "吴玉莹"
   },
   {
     "phone": "No.41",
-    "name": "幸运宝宝"
+    "name": "肖硕"
   },
   {
     "phone": "No.42",
-    "name": "幸运宝宝"
+    "name": "宋琦骏"
   },
   {
     "phone": "No.43",
-    "name": "幸运宝宝"
+    "name": "徐姗"
   },
   {
     "phone": "No.44",
-    "name": "幸运宝宝"
+    "name": "揭少凡"
   },
   {
     "phone": "No.45",
-    "name": "幸运宝宝"
+    "name": "谭慧文"
   },
   {
     "phone": "No.46",
-    "name": "幸运宝宝"
+    "name": "陈珊珊"
   },
   {
     "phone": "No.47",
-    "name": "幸运宝宝"
+    "name": "方晨"
   },
   {
     "phone": "No.48",
-    "name": "幸运宝宝"
+    "name": "万佳奇"
   },
   {
     "phone": "No.49",
-    "name": "幸运宝宝"
+    "name": "陈思瑜"
   },
   {
     "phone": "No.50",
-    "name": "幸运宝宝"
+    "name": "陈德宇"
   },
   {
     "phone": "No.51",
-    "name": "幸运宝宝"
+    "name": "秦娜娜"
   },
   {
     "phone": "No.52",
-    "name": "幸运宝宝"
+    "name": "闫兆普"
   },
   {
     "phone": "No.53",
-    "name": "幸运宝宝"
+    "name": "吴娟"
   },
   {
     "phone": "No.54",
-    "name": "幸运宝宝"
+    "name": "王辉"
   },
   {
     "phone": "No.55",
-    "name": "幸运宝宝"
+    "name": "郭容非"
   },
   {
     "phone": "No.56",
-    "name": "幸运宝宝"
+    "name": "王慧"
   },
   {
     "phone": "No.57",
-    "name": "幸运宝宝"
+    "name": "秦芳"
   },
   {
     "phone": "No.58",
-    "name": "幸运宝宝"
+    "name": "苗延兵"
   },
   {
     "phone": "No.59",
-    "name": "幸运宝宝"
+    "name": "何思聪"
   },
   {
     "phone": "No.60",
-    "name": "幸运宝宝"
+    "name": "郝建强"
   },
   {
     "phone": "No.61",
-    "name": "幸运宝宝"
+    "name": "姚妍"
   },
   {
     "phone": "No.62",
-    "name": "幸运宝宝"
+    "name": "寇小君"
   },
   {
     "phone": "No.63",
-    "name": "幸运宝宝"
+    "name": "刘爱红"
   },
+  {
+    "phone": "No.64",
+    "name": "李菲"
+  },
+  {
+    "phone": "No.65",
+    "name": "宋超"
+  }
 ]
